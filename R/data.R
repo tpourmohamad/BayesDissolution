0841b9c200545d98d3a8a21c7f61f519c6e2e78b
tpourmohamad <48219863+tpourmohamad@users.noreply.github.com>
2022-08-17 14:52:51 -0700

    Add files via upload

---
+++ R/data.R
@@ -0,0 +1,22 @@
+#' A dissolution data set taken from Ocana et al. (2009).
+#'
+#' A dissolution data set that consists of dissolution measurements
+#' taken on oral tablets made with metoclopramide hydrochloride. Of interest is
+#' to test the similarity of metoclopramide hydrochloride tablets made with and without the
+#' ingredient tensioactive.
+#'
+#' @format A data frame with 24 rows and 9 columns:
+#' \describe{
+#'   \item{group}{An indicator of whether the dissolution run came from the reference or test group}
+#'   \item{X1}{The first time point at which measurements are made at.}
+#'   \item{X2}{The second time point at which measurements are made at.}
+#'   \item{X3}{The third time point at which measurements are made at.}
+#'   \item{X4}{The fourth time point at which measurements are made at.}
+#'   \item{X5}{The fifth time point at which measurements are made at.}
+#'   \item{X6}{The sixth time point at which measurements are made at.}
+#'   \item{X7}{The seventh time point at which measurements are made at.}
+#'   \item{X8}{The eight time point at which measurements are made at.}
+#'   ...
+#' }
+#' @source \url{https://www.sciencedirect.com/science/article/abs/pii/S0169743909001488}
+"dis_data"
